b36dafe4ed579afd6b52fb8b4a0ae0457c23687e
Sri-0615 <142226546+Sri-0615@users.noreply.github.com>
2023-08-15 16:20:20 +0530

    Table.js

--- Table.js
+++ Table.js
@@ -1,87 +1,131 @@
-import React from 'react'
-function Table()
-{
-  
-    return(
-        <><main style={{marginTop: "70px", marginLeft:"200px"}}>
-     
+import React, { useState } from 'react';
+import Modal from './Modal';
 
-<div class="d-flex justify-content-between">
-  <div class="p-2"><h2>My Staff</h2></div>
-  <div class="p-2">Flex item 2</div>
-  <div class="p-2"><div class="modal-body">
-  <h5>Popover in a modal</h5>
-  <p>This <a href="#" role="button" class="btn btn-secondary popover-test" title="Popover title" data-bs-content="Popover body content is set in this attribute.">button</a> triggers a popover on click.</p>
-  <hr/>
-  <h5>Tooltips in a modal</h5>
-  <p><a href="#" class="tooltip-test" title="Tooltip">This link</a> and <a href="#" class="tooltip-test" title="Tooltip">that link</a> have tooltips on hover.</p>
-</div></div></div>
-  
-        <table class="table">
-  <thead className="table-light">
-    <tr>
-      <th>Sr No.</th>
-      <th>Name</th>
-      <th>Class</th>
-      <th>Percentage</th>
-      </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td>Gouri</td>
-      <td>5th</td>
-      </tr>
-      <tr>
-      <td>1</td>
-      <td>Gouri</td>
-      <td>5th</td>
-      </tr>
-      <tr>
-      <td>1</td>
-      <td>Gouri</td>
-      <td>5th</td>
-      </tr>
-      <tr>
-      <td>1</td>
-      <td>Gouri</td>
-      <td>5th</td>
-      </tr>
-      <tr>
-      <td>1</td>
-      <td>Gouri</td>
-      <td>5th</td>
-      </tr>
-      <tr>
-      <td>1</td>
-      <td>Gouri</td>
-      <td>5th</td>
-      </tr>
-      <tr>
-      <td>1</td>
-      <td>Gouri</td>
-      <td>5th</td>
-      </tr>
-      <tr>
-      <td>1</td>
-      <td>Gouri</td>
-      <td>5th</td>
-      </tr>
-       <tr>
-      <td>1</td>
-      <td>Gouri</td>
-      <td>5th</td>
-      </tr>
-  </tbody>
-  <tfoot>
-    <tr>
-      <td>2</td>
-      <td>Sristi</td>
-      <td>8th</td>
-      </tr>
-  </tfoot>
-</table>
-        </main></>
-    )
+function Table() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [data, setData] = useState([]);
+  const [name, setName] = useState('');
+  const [work, setWork] = useState('');
+  const [salary, setSalary] = useState('');
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const addData = () => {
+    const newData = { name, work, salary };
+    setData([...data, newData]);
+    closeModal();
+  };
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleWorkChange = (event) => {
+    setWork(event.target.value);
+  };
+
+  const handleSalaryChange = (event) => {
+    setSalary(event.target.value);
+  };
+
+  return (
+    <div>
+      <main style={{ marginTop: "70px", marginLeft: "200px" }}>
+        <div className="d-flex justify-content-between">
+          <div className="p-2">
+            <h2>My Staff</h2>
+          </div>
+          <div className="p-2">
+           
+          </div>
+          <div className="p-2">
+            <button className="btn btn-primary" onClick={openModal}>
+              Open Modal
+            </button>
+          </div>
+        </div><br/>
+{/*table for to add new data*/}
+        <table className="table">
+          <thead className="table-light">
+            <tr>
+              <th>Sr No.</th>
+              <th>Name</th>
+              <th>Work</th>
+              <th>Salary</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{item.name}</td>
+                <td>{item.work}</td>
+                <td>{item.salary}</td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td>1</td>
+              <td>Aadhar</td>
+              <td>driver</td>
+              <td>7000</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Aadhar</td>
+              <td>driver</td>
+              <td>7000</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Aadhar</td>
+              <td>driver</td>
+              <td>7000</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Aadhar</td>
+              <td>driver</td>
+              <td>7000</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Aadhar</td>
+              <td>driver</td>
+              <td>7000</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Aadhar</td>
+              <td>driver</td>
+              <td>7000</td>
+            </tr>
+          </tfoot>
+        </table>
+
+        <Modal
+  isOpen={isModalOpen}
+  onClose={closeModal}
+  name={name}
+  work={work}
+  salary={salary}
+  handleNameChange={handleNameChange}
+  handleWorkChange={handleWorkChange}
+  handleSalaryChange={handleSalaryChange}
+  addData={addData}
+>
+
+                 </Modal>
+      </main>
+    </div>
+  );
 }
+
 export default Table;
